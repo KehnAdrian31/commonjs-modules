7775e58644bc66bc61a7e1e1d0ee272014a799b6
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-29 17:55:59 +0200

    [upd] fix people.js

--- js/people.js
+++ js/people.js
@@ -1,5 +1,15 @@
+// importo la funzione
 const fullName = require('./names.js');
-console.log(fullName("Kehn","Panganiban"))
+// console.log(fullName("Kehn","Panganiban"))
 
+// importo la funzione
 const hobbies = require('./hobbies.js');
-console.log(hobbies('tennis','badminton','basketball'));
+// console.log(hobbies('tennis','badminton','basketball'));
+
+const people = () => {
+  return{
+    fullName: fullName('Kehn', 'Panganiban'),
+    hobbies: hobbies('tennis', 'badminton', 'basketball') 
+  }
+}
+console.log(people())
